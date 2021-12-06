9f63bdb0fa02318b99cbf66faf994a9fadf70908
EverSilverio <ever.silverio@gmail.com>
2021-12-06 16:57:06 -0300

    Ajustes para permitir enviar & na string do QrCode

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var url = 'https://github.com/EverSilverio';
-var apiChart = 'https://chart.googleapis.com/chart?chs=350x350&cht=qr&chl=';
+var apiChart = 'https://chart.googleapis.com/chart?chs=350x350&cht=qr&choe=UTF-8&chl=';
 var qrcodeUrl = apiChart + url;
 
 
 function loadQrCode() {
     let qrcode = document.querySelector('#qrcode');
     let inputUrl = document.querySelector('#url');
+    let urlQrCode = document.querySelector('#urlQrCode');
 
     url = inputUrl.value;
+    urlQrCode.innerHTML = url;
+
+    url = url.replace('&', '%26');
     qrcodeUrl = apiChart + url;
 
     console.log(qrcodeUrl);
